7ac8fe0a73c5965d11b2734fc64cf2ee72ddfb38
vagrant <vagrant@homestead>
2019-09-07 13:28:19 +0000

    Init

---
+++ IppiesTicketVeiling.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace Ippies\VendorCode;
+use Exception;
+
+/**
+ * Class TicketVeiling
+ *
+ * Example code for TicketVeiling for the connection
+ * with the ippies.nl api
+ *
+ * @package Ippies\VendorCode
+ * @author  Stef [stef@marshmallow.dev]
+ */
+class IppiesTicketVeiling
+{
+    /**
+     * @param string $client_id
+     * Client id to connect to the ippies.nl Api.
+     */
+    private $client_id = '{{ CLIENT_ID }}';
+
+
+    /**
+     * @param string $client_secret
+     * Client secret to connect to the ippies.nl Api.
+     */
+    private $client_secret = '{{ CLIENT_SECRET }}';
+
+
+    /**
+     * @param object/json $response
+     * This will hold the last response accessible
+     * with ->getResponse();
+     */
+    private $response;
+
+
+    /**
+     * connectAccount
+     * Connect a Ticketveiling.nl account with an ippies.nl
+     * account.
+     * 
+     * @param string $ticketveiling_customer_id
+     * @param string $ippies_nl_email_address
+     * @return \Ippies\VendorCode\IppiesTicketVeiling
+     * @throws Exception
+     */
+    public function connectAccount ($ticketveiling_customer_id, $ippies_nl_email_address)
+    {
+    	try {
+    		$response = $this->doSecureApiRequest('/tv/user/connect', [
+    			'id' => $ticketveiling_customer_id,
+	            'email' => $ippies_nl_email_address,
+    		]);
+    		return $this;
+
+    	} catch (Exception $e) {
+    		$this->errorHandler($e);
+    	}
+    }
+
+
+    /**
+     * disconnectAccount
+     * Disconnect a Ticketveiling.nl account from an
+     * ippies.nl account.
+     * 
+     * @param string $ticketveiling_customer_id
+     * @return \Ippies\VendorCode\IppiesTicketVeiling
+     * @throws Exception
+     */
+    public function disconnectAccount ($ticketveiling_customer_id)
+    {
+    	try {
+    		$response = $this->doSecureApiRequest('/tv/user/disconnect', [
+    			'id' => $ticketveiling_customer_id,
+    		]);
+    		return $this;
+
+    	} catch (Exception $e) {
+    		$this->errorHandler($e);
+    	}
+    }
+
+
+    /**
+     * newBid
+     * Add a reward to the ippies account of the
+     * ticketveiling user after a bid.
+     * 
+     * @param string $ticketveiling_customer_id
+     * @param string $ticketveiling_bid_id
+     * @param int $amount_in_euro_cents default|1
+     * @return \Ippies\VendorCode\IppiesTicketVeiling
+     * @throws Exception
+     */
+    public function newBid ($ticketveiling_customer_id, $ticketveiling_bid_id, $amount_in_euro_cents = 1)
+    {
+    	try {
+    		$response = $this->doSecureApiRequest('/tv/transaction', [
+    			'id' => $ticketveiling_customer_id,
+    			'order_id' => $ticketveiling_bid_id,
+    			'amount' => $amount_in_euro_cents,
+    		]);
+    		return $this;
+
+    	} catch (Exception $e) {
+    		$this->errorHandler($e);
+    	}
+    }
+
+
+    /**
+     * auctionWon
+     * Add a reward to the ippies account of the
+     * ticketveiling user winning an auction.
+     * 
+     * @param string $ticketveiling_customer_id
+     * @param string $ticketveiling_auction_id
+     * @param int $auction_price_in_euro_cents default|0
+     * @return \Ippies\VendorCode\IppiesTicketVeiling
+     * @throws Exception
+     */
+    public function auctionWon ($ticketveiling_customer_id, $ticketveiling_auction_id, $auction_price_in_euro_cents = 0)
+    {
+    	try {
+    		$response = $this->doSecureApiRequest('/tv/auctionwon', [
+    			'id' => $ticketveiling_customer_id,
+    			'order_id' => $ticketveiling_auction_id,
+    			'order_value' => $auction_price_in_euro_cents,
+    		]);
+    		return $this;
+
+    	} catch (Exception $e) {
+    		$this->errorHandler($e);
+    	}
+    }
+
+
+    /**
+     * getAccessToken
+     * Get the access token needed for secure
+     * api calls to the ippies.nl Rest Api.
+     * 
+     * @return string $access_token
+     * @throws Exception
+     */
+    private function getAccessToken ()
+    {
+        $response = $this->doApiRequest('/grant', [
+            'grant_type' => 'client_credentials',
+            'client_id' => $this->client_id,
+            'client_secret' => $this->client_secret,
+        ]);
+
+        if (!$response || !isset($response->access_token)) {
+        	throw new Exception("Ippies/Vendor/Ticketveiling: Error getting an access token", 1);	
+        }
+        return $response->access_token;
+    }
+
+
+    /**
+     * doSecureApiRequest
+     * Do a secure api request. This will automatically add
+     * the access token to the Bearer header.
+     * 
+     * @param string $endpoint
+     * @param array $request_data
+     * @return object/json $response
+     */
+    private function doSecureApiRequest ($endpoint, $request_data)
+    {
+    	return $this->doApiRequest($endpoint, $request_data, [
+    		"Authorization: Bearer " . $this->getAccessToken(),
+    	]);
+    }
+
+
+    /**
+     * doApiRequest
+     * Get the response from the ippies.nl Rest Api.
+     * 
+     * @param string $endpoint
+     * @param array $request_data
+     * @param array $extra_headers
+     * @return object/json $response
+     */
+    private function doApiRequest ($endpoint, $request_data, $extra_headers = [])
+    {
+    	$request_data_json = json_encode($request_data);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://rest.ippies.nl" . $endpoint,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => $request_data_json,
+            CURLOPT_HTTPHEADER => array_merge([
+                "Accept: */*",
+                "Cache-Control: no-cache",
+                "Content-Length: " . strlen($request_data_json),
+                "Content-Type: application/json",
+            ], $extra_headers)
+        ]);
+
+        $response = curl_exec($curl);
+        $response = json_decode($response);
+        $this->setResponseInformation($response);
+        return $response;
+    }
+
+
+    /**
+     * getResponse
+     * Get the latest response from the api. This can be
+     * used the validate if the request was successfull.
+     * 
+     * @return object/json $response
+     */
+    public function getResponse ()
+    {
+    	return $this->response;
+    }
+
+
+    /**
+     * setResponseInformation
+     * Set the latest Api response in a variable so it can
+     * be accessed with the getResponse() method.
+     * 
+     * @param object/json $response
+     * @return void
+     */
+    private function setResponseInformation ($response)
+    {
+    	$this->response = $response;
+    }
+
+
+    /**
+     * errorHandler
+     * This will handle errors will contacting the ippies.nl
+     * Rest Api. By default the method will write to the server
+     * error log. If you want to handle errors differently, feel
+     * free to adjust this method to your liking.
+     * 
+     * @param Exception $exception
+     * @return void
+     */
+    private function errorHandler (Exception $exception)
+    {
+    	error_log($exception->getMessage());
+    }
+}
---
+++ README.md
@@ -0,0 +1,124 @@
+![alt text](https://www.ippies.nl/images/ippies-logo-header.png "ippies.nl")
+
+# Ticketveiling & ippies.nl
+This class can be used for easy integration to the ippies.nl Rest Api. All current endpoints available to Ticketveiling are available in this class. Please see the usage and information below. This update is very important because the current API that is based on SOAP is going to be phased out in 2019 because we are unable to keep the old API save and stable.
+
+- [Usage](#usage) 
+- [Available methods](#available_methodes) 
+    - [connectAccount](#method_connectAccount) 
+    - [disconnectAccount](#method_disconnectAccount)
+    - [newBid](#method_newBid)
+    - [auctionWon](#method_auctionWon)
+- [Ticketveiling Integration](#ticketveiling_integration) 
+- [PHP versions](#php_versions) 
+
+<a id="usage"></a>
+## Usage
+First of all, you need to require `IppiesTicketVeiling.php` so the code is available to us. You are free to change the file name as required.
+```php 
+$ticketveiling_class = 'src/Ippies/VendorCode/IppiesTicketVeiling.php';
+require_once $ticketveiling_class;
+```
+
+Next you need to add your api keys to the `IppiesTicketVeiling.php` class. Open the file and add it in the private variables.
+```php
+<?php
+
+...
+
+class IppiesTicketVeiling
+{
+    private $client_id = '{{ CLIENT_ID }}';
+    private $client_secret = '{{ CLIENT_SECRET }}';
+
+    ...
+
+```
+
+<a id="available_methodes"></a>
+#### Available methodes
+1. [connectAccount](#method_connectAccount)
+2. [disconnectAccount](#method_disconnectAccount)
+3. [newBid](#method_newBid)
+4. [auctionWon](#method_auctionWon)
+
+<a id="method_connectAccount"></a>
+`connectAccount`:<br/> 
+Connect a Ticketveiling account to an ippies account:
+```php 
+$ticketveiling_customer_id = 645757;
+$ippies_nl_email_address = 'stef@marshmallow.dev';
+(new \Ippies\VendorCode\IppiesTicketVeiling)->connectAccount($ticketveiling_customer_id, $ippies_nl_email_address);
+```
+
+<a id="method_disconnectAccount"></a>
+`disconnectAccount`:<br/> 
+Disconnect a Ticketveiling account from an ippies account:
+```php 
+$ticketveiling_customer_id = 645757;
+(new \Ippies\VendorCode\IppiesTicketVeiling)->disconnectAccount($ticketveiling_customer_id);
+```
+
+<a id="method_newBid"></a>
+`newBid`:<br/> 
+Give the user a reward for bidding on Ticketveiling.nl:
+```php 
+$ticketveiling_customer_id = 645757;
+$ticketveiling_bid_id = 'BID_NUMBER_###'; // The unique bid id from Ticketveiling.nl
+$amount_in_euro_cents = 1; // Not required, this will be 1 by default.
+(new \Ippies\VendorCode\IppiesTicketVeiling)->newBid(
+    $ticketveiling_customer_id, 
+    $ticketveiling_bid_id,
+    $amount_in_euro_cents
+);
+```
+
+<a id="method_auctionWon"></a>
+`auctionWon`:<br/> 
+Give the user a reward for winning an auction on Ticketveiling.nl:
+```php 
+$ticketveiling_customer_id = 645757;
+$ticketveiling_auction_id = 'AUCTION_NUMBER_###'; // The unique auction id from Ticketveiling.nl
+$auction_price_in_euro_cents = 0; // Not required, this will be 0 by default.
+(new \Ippies\VendorCode\IppiesTicketVeiling)->newBid(
+    $ticketveiling_customer_id, 
+    $ticketveiling_auction_id,
+    $auction_price_in_euro_cents
+);
+```
+
+<a id="ticketveiling_integration"></a>
+## Ticketveiling integration
+Based on code we've have seen in the past we would like to provide a simple snippet to get Ticketveiling started with the new integration with the ippies.nl Rest Api.
+
+<strong>MijnVeilingPage.php</strong>
+```php
+<?php
+
+$ticketveiling_class = 'src/Ippies/VendorCode/IppiesTicketVeiling.php';
+require_once $ticketveiling_class;
+
+//...
+
+public function connectAccount ($TVId, $emailI) {
+    (new \Ippies\VendorCode\IppiesTicketVeiling)->connectAccount($TVId, $emailI);
+}
+
+//...
+
+public function disconnectAccount ($TVId) {
+    (new \Ippies\VendorCode\IppiesTicketVeiling)->disconnectAccount($TVId);
+}
+
+```
+
+<a id="php_versions"></a>
+## PHP versions
+This class has been tested in the PHP versions listed below. If you are using a different PHP version and want us to test it for you, please let us know at lars@ippies.nl and provide the PHP version that you need tested.
+
+- [x] PHP 5.4.45
+- [x] PHP 5.6.40
+- [x] PHP 7.0.33
+- [x] PHP 7.1.26
+- [x] PHP 7.2.14
+- [x] PHP 7.3.1
